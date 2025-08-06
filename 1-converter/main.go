2159d42c19833fae31f141b84f41f01c05cd623b
Daniil R <d.ruzin@pikabu.ru>
2025-08-06 22:14:51 +0300

    Refactor currency conversion logic to directly calculate RUB from EUR and update output format in main.go

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -4,10 +4,10 @@ import "fmt"
 
 const USD2EUR = 0.86
 const USD2RUB = 0.8
+const EUR2RUB = USD2RUB / USD2EUR
 
 func main() {
 	eurInput := 100.0
-	usd := eurInput / USD2EUR
-	rub := usd * USD2RUB
-	fmt.Printf("%.2f EUR is %.2f USD and %.2f RUB\n", eurInput, usd, rub)
+	rub := eurInput * EUR2RUB
+	fmt.Printf("%.2f EUR is %.2f RUB\n", eurInput, rub)
 }
