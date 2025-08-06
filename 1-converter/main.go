68912c465635414061925f830eafbc3df51b4eb5
Daniil R <d.ruzin@pikabu.ru>
2025-08-06 22:11:37 +0300

    Refactor currency conversion calculation and improve output formatting in main.go

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -7,10 +7,7 @@ const USD2RUB = 0.8
 
 func main() {
 	eurInput := 100.0
-
-	usd := (1.0 / USD2EUR) * eurInput
-
+	usd := eurInput / USD2EUR
 	rub := usd * USD2RUB
-
-	fmt.Println(rub)
+	fmt.Printf("%.2f EUR is %.2f USD and %.2f RUB\n", eurInput, usd, rub)
 }
