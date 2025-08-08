ab9cd53fdeaeceb370fafeeac6c7ea5217294f75
Daniil R <d.ruzin@pikabu.ru>
2025-08-08 19:56:53 +0300

    Implement user input functionality for EUR to RUB conversion in main.go

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -7,7 +7,19 @@ const USD2RUB = 0.8
 const EUR2RUB = USD2RUB / USD2EUR
 
 func main() {
-	eurInput := 100.0
+	eurInput := getUserInput()
 	rub := eurInput * EUR2RUB
 	fmt.Printf("%.2f EUR is %.2f RUB\n", eurInput, rub)
+}
+
+func getUserInput() float64 {
+	var eurInput float64
+
+	fmt.Println("Введите сумму в евро:")
+	fmt.Scan(&eurInput)
+
+	return eurInput
+}
+
+func convert(amount float64, fromCurrency string, toCurrency string) float64 {
 }
